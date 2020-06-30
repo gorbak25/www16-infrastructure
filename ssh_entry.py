676d3ed7d137d33dc46b8a454e5e212c0c18904c
Grzegorz Uriasz <gorbak25@gmail.com>
2020-06-30 11:21:17 +0200

    s/replate/replace

--- ssh_entry.py
+++ ssh_entry.py
@@ -123,7 +123,7 @@ def banhammer(user):
 
 def render_deployment_template(user, user_rootfs):
     template = open(os.path.join(this_dir, "k8s/www16-user.yaml.template"), "r").read()
-    return template.replate("$WWW_USER", user).replace("$WWW_USER_ROOTFS", user_rootfs)
+    return template.replace("$WWW_USER", user).replace("$WWW_USER_ROOTFS", user_rootfs)
 
 if 'SSH_ORIGINAL_COMMAND' not in os.environ:
     help()
